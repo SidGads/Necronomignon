6c092988d094f867e535bdff8e36adcbc648881b
SidGads <sidneygad@hotmail.com>
2020-08-31 15:16:23 -0400

    Revert "small change"
    This reverts commit 2dcee93ae6c19d210df97911ed2621f50c3c8d66.

--- Assets/Scripts/data/BeastManager.cs
+++ Assets/Scripts/data/BeastManager.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +8,6 @@ namespace Assets.Scripts.data
 {
     class BeastManager
     {
-        private String con = ConfigurationManager.ConnectionStrings["Yourconnection"].ToString();
-
-
-        using (SqlConnection myConnection = new SqlConnectionStringBuilder(con)){
 
     }
 }
