282e9b39c28cbf2f3591818b57696708a0816463
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2020-09-20 20:29:37 -0400

    Battle scene, load animations
    New battle scene. Idle animation and Damaged animation hard coded in.

---
+++ Assets/LoadTeam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadTeam : MonoBehaviour
+{
+    Animator anim;
+    GameObject slot1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slot1 = gameObject.transform.GetChild(0).gameObject;
+        anim = slot1.GetComponent<Animator>();
+        anim.runtimeAnimatorController = Resources.Load("Animations/Abyssal/Idle/Idle") as RuntimeAnimatorController;
+        anim.speed = 3;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            anim.runtimeAnimatorController = Resources.Load("Animations/Abyssal/Damaged/Damaged") as RuntimeAnimatorController;
+            anim.speed = 2;
+        }
+    }
+}
